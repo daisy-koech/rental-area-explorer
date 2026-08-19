8a575be8a51248848c520b8acf418cf55ac403d2
Daisy Koech <koechdaisy86@gmail.com>
2026-08-19 10:39:32 +0300

    add application navigation

---
+++ src/views/AmenitiesView.jsx
@@ -0,0 +1,10 @@
+function AmenitiesView() {
+    return (
+      <div>
+        <h1>Amenities</h1>
+        <p>Nearby schools, hospitals, transport and markets will appear here.</p>
+      </div>
+    );
+  }
+
+export default AmenitiesView;
